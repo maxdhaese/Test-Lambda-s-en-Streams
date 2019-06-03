ec1ff7581865f04cce3b07d1c1491f0f531926f1
maxdhaese <maxdhaese@gmail.com>
2019-06-03 13:20:47 +0200

    commit

--- src/be/intecbrussel/lambdatest/model/MeasurementApp.java
+++ src/be/intecbrussel/lambdatest/model/MeasurementApp.java
@@ -1,11 +1,16 @@
 package be.intecbrussel.lambdatest.model;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.OptionalInt;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MeasurementApp {
     public static void main(String[] args) {
-        generateMeasurementArray(3).toString();
+
+        printHighestTemperature(generateMeasurementArray(3));
 
 
 
@@ -25,12 +30,23 @@ public class MeasurementApp {
     }
 
     public static void printHighestTemperature(SensorMeasurement[] sensorMeasurements) {
+        Optional<BigDecimal> max = Stream.of(sensorMeasurements)
+                .map(s->s.getTemperatureFahrenheit())
+                .max(BigDecimal::compareTo);
+
+        if (max.isPresent()){
+            Stream.of(sensorMeasurements)
+                    .forEach(System.out::println);
+            System.out.printf("The maximum temperature in Celcius is: %.1f degrees\n In Fahrenheit :%.1f F",max.get(),max.get().multiply(new BigDecimal(1.8)).add(BigDecimal.valueOf(32)));
+        }
 
 
     }
 
     public static void printSortedByLightIntensity(SensorMeasurement[] sensorMeasurements) {
-
+        Stream.of(sensorMeasurements)
+                .sorted()
+                .forEach(System.out::println);
     }
 
     public static String[] mapToInfoString(SensorMeasurement[] sensorMeasurements) {
