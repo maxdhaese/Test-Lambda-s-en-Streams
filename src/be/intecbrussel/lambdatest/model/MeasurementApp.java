dde95e0d1c1bbe0a2a81c1ab850257454add5985
maxdhaese <maxdhaese@gmail.com>
2019-06-03 14:20:13 +0200

    commit

--- src/be/intecbrussel/lambdatest/model/MeasurementApp.java
+++ src/be/intecbrussel/lambdatest/model/MeasurementApp.java
@@ -1,8 +1,10 @@
 package be.intecbrussel.lambdatest.model;
 
+import javax.swing.text.html.Option;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -10,7 +12,18 @@ import java.util.stream.Stream;
 public class MeasurementApp {
     public static void main(String[] args) {
 
+        System.out.println("---Print highest temperature---");
         printHighestTemperature(generateMeasurementArray(3));
+        System.out.println();
+
+        System.out.println("---Print sorted by lightIntensity---");
+        printSortedByLightIntensity(generateMeasurementArray(3));
+
+        System.out.println("---Print average Temperature");
+        printAverageTemperature(generateMeasurementArray(3));
+
+
+
 
 
 
@@ -37,7 +50,7 @@ public class MeasurementApp {
         if (max.isPresent()){
             Stream.of(sensorMeasurements)
                     .forEach(System.out::println);
-            System.out.printf("The maximum temperature in Celcius is: %.1f degrees\n In Fahrenheit :%.1f F",max.get(),max.get().multiply(new BigDecimal(1.8)).add(BigDecimal.valueOf(32)));
+            System.out.printf("The average temperature in Celcius is: %.1f degrees and in Fahrenheit :%.1f F",max.get(),max.get().multiply(new BigDecimal(1.8)).add(BigDecimal.valueOf(32)));
         }
 
 
@@ -45,16 +58,38 @@ public class MeasurementApp {
 
     public static void printSortedByLightIntensity(SensorMeasurement[] sensorMeasurements) {
         Stream.of(sensorMeasurements)
+                    .forEach(System.out::println);
+        Stream.of(sensorMeasurements)
+                .map(s->s.getLightIntensity())
                 .sorted()
                 .forEach(System.out::println);
+
+
+
     }
 
     public static String[] mapToInfoString(SensorMeasurement[] sensorMeasurements) {
+        Stream<SensorMeasurement> stream =Arrays.stream(sensorMeasurements)
+                .forEach(str-> System.out.printf("humidity: %1.f; LightIntensity: %d lumen; Temperature: %.1f",str.getHumidity(),str.getLightIntensity(),str.getTemperatureCelcius()));
+
+
+
+
+
         return null;
 
     }
 
     public static void printAverageTemperature(SensorMeasurement[] sensorMeasurements) {
+        OptionalDouble avg = Stream.of(sensorMeasurements)
+                .mapToDouble(s->s.getTemperatureFahrenheit().doubleValue())
+                .average();
+
+        if (avg.isPresent()){
+            Stream.of(sensorMeasurements)
+                    .forEach(System.out::println);
+            System.out.printf("The maximum temperature in Celcius is: %.1f degrees and in Fahrenheit :%.1f F",avg.getAsDouble(),(avg.getAsDouble()*1.8+32));
+        }
 
     }
 
